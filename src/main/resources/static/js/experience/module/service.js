ba2d5248b42f2acf9453e2b7c87ec6f0bf46a59a
Eunyu <wjddmsdb03@gmail.com>
2025-11-18 13:40:51 +0900

    test6

--- src/main/resources/static/js/experience/module/service.js
+++ src/main/resources/static/js/experience/module/service.js
@@ -10,7 +10,7 @@ const experienceService = (() => {
     };
 
     const getRecommendNotice=async ()=>{
-        const url=`https://shares-distributor-physics-pens.trycloudflare.com/api/user-experience`;
+        const url=` https://trend-recruiting-shoes-nokia.trycloudflare.com/api/user-experience`;
         const memberId=user.id;
         const response=await fetch(url, {
             method: "POST",
